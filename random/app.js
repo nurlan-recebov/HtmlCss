3a6bcb3daa653324b16af69c62effa5d97662d30
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-03-18 21:18:45 +0400

    yeni task

---
+++ random/app.js
@@ -0,0 +1,12 @@
+let a = 0
+let x = Math.random(a) * 10
+let n = alert(x.toFixed() * 14);
+let b = 0
+let y = Math.random(a) * 10
+let m = alert(y.toFixed() * 14);
+let z = Number(n) + Number(m)
+let d = prompt(Cavab)
+if (z) {
+    alert(n)
+
+}
