d7f14298ae4563261b55a9d24c493e29892028c2
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-03-18 19:50:29 +0400

    yeni taskall

---
+++ alert/app.js
@@ -0,0 +1,9 @@
+let a = prompt("a ededi daxil edin")
+let b = prompt("b ededi daxil edin")
+let cavab = prompt("cavab daxil edin")
+let toplama = Number(a) + Number(b)
+if (cavab == toplama) {
+    alert("duzdur")
+} else {
+    alert("sehvdir")
+}
